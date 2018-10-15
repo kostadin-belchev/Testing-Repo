efea20b69ac5827c2dcdb615f004d0dae602fd90
kostadin-belchev <kostadin.belchev@gmail.com>
2018-10-15 16:26:04 +0300

    adding some more lines of test code

---
+++ Redux.js
@@ -0,0 +1,11 @@
+// const list = [10, 11, 20]
+
+// const incrementCounter = (list, index) => {
+//   return [
+//     ...list.slice(0, index),
+//     list[index] + 1,
+//     ...list.slice(index + 1)
+//   ]
+// }
+
+// console.log(incrementCounter(list, 1))
--- UnderstandingECMAScript6.js
+++ UnderstandingECMAScript6.js
@@ -1,10 +1,48 @@
-function getFlags(re) {
-  var text = re.toString();
-  text.substring()
-  return text.substring(text.lastIndexOf("/") + 1);
-}
+let node = {
+  type: "Identifier",
+  name: "foo",
+  loc: {
+      start: {
+          line: 1,
+          column: 1
+      },
+      end: {
+          line: 1,
+          column: 4
+      }
+  }
+};
 
-// toString() is "/ab/g"
-var re = /ab/giu;
+let { type, loc: { end: localEnd }, value: localValue = 'default local value' } = node
+console.log(localEnd.column)
+console.log(localValue)
 
-console.log(getFlags(re));  
+// let node = {
+//   type: 'Indentifier',
+//   name: 'foo'
+// }
+// let type = 'Literal'
+// let name = 5;
+
+// function outputInfo({ type: localType, name: localName, value: localValue = 'default local value' }) {
+//   console.log(localType)
+//   console.log(localName)
+//   console.log(localValue)
+// }
+
+// outputInfo(node)
+
+// console.log(type)
+// console.log(name)
+
+let colors = [ "red", [ "green", "lightgreen" ], "blue" ];
+
+// later
+
+let [ firstColor, [ secondColor, thirdColor, fourthColor = 'purple' ], sixthColor ] = colors;
+
+console.log(firstColor);        // "red"
+console.log(secondColor);       // "green"
+console.log(thirdColor)
+console.log(fourthColor)
+console.log(sixthColor)
